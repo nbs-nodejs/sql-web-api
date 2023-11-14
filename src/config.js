8e3a6f27b39ed1c4f85700490c40ac233aa019fb
Saggaf <saggaf.arsyad@gmail.com>
2023-11-14 21:31:01 +0700

    feat: Add postgres support

--- src/config.js
+++ src/config.js
@@ -14,9 +14,13 @@ const config = {
         pass: process.env.DB_PASS,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
+        sslMode: process.env.DB_SSL === "true",
         sqlite: {
             filename: process.env.DB_SQLITE_FILENAME
         },
+        postgres: {
+            searchPath: process.env.DB_POSTGRES_SEARCH_PATH,
+        }
     }
 }
 
--- src/db.js
+++ src/db.js
@@ -7,6 +7,14 @@ function initDb(db) {
     }
 
     switch (db.driver) {
+        case "postgres": {
+            option.client = "pg"
+            option.connection = `postgresql://${db.user}:${db.pass}@${db.host}:${db.port}/${db.name}?useSSL=${db.sslMode}`
+            if (db.postgres.searchPath) {
+                option.searchPath = db.postgres.searchPath.split(",")
+            }
+            break
+        }
         case "sqlite3":
         case "better-sqlite3": {
             option.connection = {
