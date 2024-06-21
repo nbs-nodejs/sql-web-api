ff53430edb50d766fbe45801ba07b8f098fb9919
Saggaf <saggaf.arsyad@gmail.com>
2024-06-21 20:59:13 +0700

    feat: Add API Update by Condition and refactor where query builder

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,7 @@ const Router = require("koa-router");
 const camelCase = require('lodash.camelcase');
 const db = require("./db");
 const logger = require("./logger");
+const ApiError = require("./api-error");
 
 /**
  * @return {module:koa-router|Router}
@@ -44,152 +45,7 @@ function initController() {
         }
 
         if (query.where) {
-            let count = 0;
-            for (const key in query.where) {
-                let value = query.where[key];
-
-                // Check if value is type, then use equal operator
-                switch (typeof value) {
-                    case "string":
-                    case "number":
-                    case "bigint":
-                    case "boolean": {
-                        count++;
-
-                        if (count === 1) {
-                            qb.where(key, value);
-                            continue;
-                        }
-
-                        qb.andWhere(key, value);
-                        continue;
-                    }
-                    case "object": {
-                        // Do nothing
-                        break;
-                    }
-                    default: {
-                        continue;
-                    }
-                }
-
-                // If value is an array, then skip
-                if (Array.isArray(value)) {
-                    continue;
-                }
-
-                // If Object does not contain any key, then skip
-                const vKeys = Object.keys(value);
-                if (vKeys.length === 0) {
-                    continue;
-                }
-
-                // If value of clause is not a string, then skip
-                let clause = vKeys[0];
-                switch (clause) {
-                    case "isNull": {
-                        count++;
-                        qb = qb.whereNull(key);
-                        continue;
-                    }
-                    case "isNotNull": {
-                        count++;
-                        if (count === 1) {
-                            qb = qb.whereNot(key, null);
-                        } else {
-                            qb = qb.andWhereNot(key, null);
-                        }
-                        continue;
-                    }
-                }
-
-                let op;
-                switch (clause) {
-                    case "gt": {
-                        op = ">";
-                        break;
-                    }
-                    case "gte": {
-                        op = ">=";
-                        break;
-                    }
-                    case "lt": {
-                        op = "<";
-                        break;
-                    }
-                    case "lte": {
-                        op = "<=";
-                        break;
-                    }
-                }
-
-                if (op) {
-                    value = value[clause];
-                    count++;
-                    if (count === 1) {
-                        qb = qb.where(key, op, value);
-                    } else {
-                        qb = qb.andWhere(key, op, value);
-                    }
-                    continue;
-                }
-
-                // TODO: Check if grouped where clause
-
-                value = value[clause];
-                if (typeof value !== "string") {
-                    continue;
-                }
-
-                switch (clause) {
-                    case "not":
-                    case "like":
-                    case "iLike":
-                    case "exists":
-                    case "notExists": {
-                        clause = camelCase("where-" + clause);
-                        if (value === "null") {
-                            value = null;
-                        }
-                        break;
-                    }
-                    case "in": {
-                        clause = "whereIn";
-                        value = value.split(",");
-                        break;
-                    }
-                    case "notIn": {
-                        clause = "whereNotIn";
-                        value = value.split(",");
-                        break;
-                    }
-                    case "between": {
-                        clause = "whereBetween";
-                        value = value.split(",", 2)
-                        if (value.length !== 2) {
-                            continue;
-                        }
-                        break;
-                    }
-                    case "notBetween": {
-                        clause = "whereNotBetween";
-                        value = value.split(",", 2)
-                        if (value.length !== 2) {
-                            continue;
-                        }
-                        break;
-                    }
-                    default: {
-                        continue;
-                    }
-                }
-
-                count++;
-                if (count > 1) {
-                    clause = camelCase("and-" + clause);
-                }
-                qb = qb[clause](key, value);
-            }
+            buildWhere(qb, query.where);
         }
 
         if (query.orderBy) {
@@ -268,7 +124,185 @@ function initController() {
         ctx.data = data;
     })
 
+    // Update by query
+    controller.put("/v1/:tableName", async (ctx, _) => {
+        const {tableName} = ctx.params;
+        const data = ctx.request.body;
+
+        // Validate where params
+        const {query} = ctx;
+        if (!query.where) {
+            throw new ApiError("400", "Query where is required", {httpStatus: 400});
+        }
+
+        // Build where query
+        let qb = db(tableName);
+        const countWhere = buildWhere(qb, query.where);
+        if (countWhere === 0) {
+            throw new ApiError("400", "Query where is required", {httpStatus: 400});
+        }
+        const result = await qb.update(data);
+
+        ctx.status = 200;
+        ctx.data = data;
+    })
+
     return controller;
+}
+
+function buildWhere(qb, conditions) {
+    if (typeof conditions !== "object" || Array.isArray(conditions)) {
+        throw new ApiError("400", "query where must be an object", {httpStatus: 400});
+    }
+
+    let count = 0;
+    for (const key in conditions) {
+        let value = conditions[key];
+
+        // Check if value is type, then use equal operator
+        switch (typeof value) {
+            case "string":
+            case "number":
+            case "bigint":
+            case "boolean": {
+                count++;
+
+                if (count === 1) {
+                    qb.where(key, value);
+                    continue;
+                }
+
+                qb.andWhere(key, value);
+                continue;
+            }
+            case "object": {
+                // Do nothing
+                break;
+            }
+            default: {
+                continue;
+            }
+        }
+
+        // If value is an array, then skip
+        if (Array.isArray(value)) {
+            continue;
+        }
+
+        // If Object does not contain any key, then skip
+        const vKeys = Object.keys(value);
+        if (vKeys.length === 0) {
+            continue;
+        }
+
+        // If value of clause is not a string, then skip
+        let clause = vKeys[0];
+        switch (clause) {
+            case "isNull": {
+                count++;
+                qb = qb.whereNull(key);
+                continue;
+            }
+            case "isNotNull": {
+                count++;
+                if (count === 1) {
+                    qb = qb.whereNot(key, null);
+                } else {
+                    qb = qb.andWhereNot(key, null);
+                }
+                continue;
+            }
+        }
+
+        let op;
+        switch (clause) {
+            case "gt": {
+                op = ">";
+                break;
+            }
+            case "gte": {
+                op = ">=";
+                break;
+            }
+            case "lt": {
+                op = "<";
+                break;
+            }
+            case "lte": {
+                op = "<=";
+                break;
+            }
+        }
+
+        if (op) {
+            value = value[clause];
+            count++;
+            if (count === 1) {
+                qb = qb.where(key, op, value);
+            } else {
+                qb = qb.andWhere(key, op, value);
+            }
+            continue;
+        }
+
+        // TODO: Check if grouped where clause
+
+        value = value[clause];
+        if (typeof value !== "string") {
+            continue;
+        }
+
+        switch (clause) {
+            case "not":
+            case "like":
+            case "iLike":
+            case "exists":
+            case "notExists": {
+                clause = camelCase("where-" + clause);
+                if (value === "null") {
+                    value = null;
+                }
+                break;
+            }
+            case "in": {
+                clause = "whereIn";
+                value = value.split(",");
+                break;
+            }
+            case "notIn": {
+                clause = "whereNotIn";
+                value = value.split(",");
+                break;
+            }
+            case "between": {
+                clause = "whereBetween";
+                value = value.split(",", 2)
+                if (value.length !== 2) {
+                    continue;
+                }
+                break;
+            }
+            case "notBetween": {
+                clause = "whereNotBetween";
+                value = value.split(",", 2)
+                if (value.length !== 2) {
+                    continue;
+                }
+                break;
+            }
+            default: {
+                continue;
+            }
+        }
+
+        count++;
+        if (count > 1) {
+            clause = camelCase("and-" + clause);
+        }
+        qb = qb[clause](key, value);
+    }
+
+    return count;
 }
 
 module.exports = initController();
